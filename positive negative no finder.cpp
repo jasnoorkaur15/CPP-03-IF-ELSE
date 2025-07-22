0e0aa3877fc7c83b47376250bdaeaaab56130693
jasnoorkaur15 <kjasnoor15@gmail.com>
2025-07-23 00:28:49 +0530

    Create positive negative no finder.cpp

---
+++ positive negative no finder.cpp
@@ -0,0 +1,28 @@
+//Jasnoor Kaur
+//24070123049
+
+#include <iostream>
+using namespace std;
+int a,b;
+
+int main() {
+    cout<< " Value of a: ";
+    cin >> a;
+    cout<< " Value of b: ";
+    cin >> b;
+    
+    if (a>0){
+        cout<< "A is a +ve number." << "\n";
+    }
+    else{
+        cout<< "A is a -ve number."<< "\n";
+    }
+    
+    if (b>0){
+        cout<< "B is a +ve number."<< "\n";
+    }
+    else{
+        cout<< "B is a -ve number."<< "\n";
+    }
+    
+    return 0;
